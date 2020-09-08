f45795ec397d3e8f68db731185c7303651e9c1e6
KeshavSingh7 <keshavsingh2037@gmail.com>
2020-09-08 21:46:30 +0530

    intermediate commit

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
 window.addEventListener("load", () => {
   const ques = document.getElementById("question");
-  const op1 = document.getElementById("option1");
-  const op2 = document.getElementById("option2");
-  const op3 = document.getElementById("option3");
-  const op4 = document.getElementById("option4");
-
-  const radioSelect = document.getElementsByClassName("radio-container");
-
-  radioSelect[0].addEventListener("click", () => {
-    radioSelect[0].querySelector("input").checked = true;
-  });
-  radioSelect[1].addEventListener("click", () => {
-    radioSelect[1].querySelector("input").checked = true;
-  });
-  radioSelect[2].addEventListener("click", () => {
-    radioSelect[2].querySelector("input").checked = true;
-  });
-  radioSelect[3].addEventListener("click", () => {
-    radioSelect[3].querySelector("input").checked = true;
-  });
+  const opc = document.getElementsByClassName("options-container");
 
   const url = "https://opentdb.com/api.php?amount=10";
   fetch(url)
@@ -26,16 +8,72 @@ window.addEventListener("load", () => {
       return response.json();
     })
     .then((data) => {
-      //console.log(data);
       let q = data.results[0].question;
-      let a = data.results[0].correct_answer;
-      let b = data.results[0].incorrect_answers[0];
-      let c = data.results[0].incorrect_answers[1];
-      let d = data.results[0].incorrect_answers[2];
+      q = q.replaceAll("&quot;", '"').replaceAll("&#039;", "'");
       ques.innerText = q;
-      op1.innerText = a;
-      op2.innerText = b;
-      op3.innerText = c;
-      op4.innerText = d;
+      let a = data.results[0].correct_answer;
+      let b = data.results[0].incorrect_answers;
+      let arr = new Array(b.length + 1);
+      let rn = Math.floor(Math.random() * (b.length + 1));
+      arr[rn] = a;
+      let c = 0;
+      for (let i = 0; i < b.length + 1; i++) {
+        if (i != rn) {
+          arr[i] = b[c++];
+        }
+      }
+
+      for (let i = 0; i < b.length + 1; i++) {
+        let x = document.createElement("div");
+        x.classList.add("radio-container");
+
+        let y = document.createElement("input");
+        y.setAttribute("type", "radio");
+        y.setAttribute("name", "option");
+        y.setAttribute("id", `radio${i}`);
+        y.setAttribute("class", "radio");
+
+        let z = document.createElement("label");
+        z.setAttribute("for", `radio${i}`);
+        z.innerText = arr[i];
+
+        let w;
+        if (i == rn) {
+          w = document.createElement("i");
+          w.setAttribute("class", "fas fa-check");
+        } else {
+          w = document.createElement("i");
+          w.setAttribute("class", "fas fa-times");
+        }
+
+        x.appendChild(y);
+        x.appendChild(z);
+        x.appendChild(w);
+        opc[0].appendChild(x);
+      }
+
+      const radioSelect = document.getElementsByClassName("radio-container");
+      for (let i = 0; i < radioSelect.length; i++) {
+        radioSelect[i].addEventListener("click", () => {
+          radioSelect[i].querySelector("input").checked = true;
+        });
+      }
+      //const radioSelect = document.getElementsByClassName("radio");
+      for (let i = 0; i < b.length + 1; i++) {
+        radioSelect[i].addEventListener("click", () => {
+          if (
+            radioSelect[i].querySelector("input").checked === true &&
+            radioSelect[i].querySelector("label").innerText === a
+          ) {
+            console.log(radioSelect[i].checked === true);
+            console.log(radioSelect[i].querySelector("label").innerText === a);
+            radioSelect[i].classList.add("correct");
+          } else {
+            console.log(radioSelect[i].checked === true);
+            console.log(radioSelect[i].querySelector("label").innerText === a);
+            radioSelect[i].classList.add("wrong");
+          }
+        });
+      }
     });
 });
